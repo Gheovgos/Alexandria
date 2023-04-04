b04be52ad7e5e27d03478515eaccf66ad24e96a8
Giuseppe <snesnopic@gmail.com>
2023-04-04 16:38:36 +0200

    Finalizzato welcome screen, ora puoi navigare al Login Screen

--- alexandria/lib/welcome_screen.dart
+++ alexandria/lib/welcome_screen.dart
@@ -1,50 +1,72 @@
 import 'package:flutter/material.dart';
+import 'alexandria_container.dart';
+import 'alexandria_rounded_button.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
+    return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Color(0xFF37C534),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(120.0),
+          Hero(
+            tag: 1,
+            child: AlexandriaContainer(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Image.asset(
+                    'assets/alexandria_name_white.png',
+                    height: 170.0,
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  )
+                ],
               ),
             ),
-            height: 300,
-            child: Center(child: Image(image: AssetImage('assets/logo.png'),)),
           ),
-          Text(
+          const Text(
             'Benvenuto su Alexandria, l’app personale per la gestione affidabile dei propri riferimenti bibliografici.',
             textAlign: TextAlign.center,
           ),
-          SizedBox(
+          const SizedBox(
             height: 100,
           ),
-          Text(
-            ' Potrai aggiungere e modificare i tuoi riferimenti, inoltre potrai visualizzare i riferimenti degli altri utenti.',
+          const Text(
+            'Potrai aggiungere e modificare i tuoi riferimenti, inoltre potrai visualizzare i riferimenti degli altri utenti.',
             textAlign: TextAlign.center,
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text(
+          AlexandriaRoundedButton(
+            padding: const EdgeInsets.all(15.0),
+            backgroundColor: Colors.white,
+            child: const Text(
               "Unisciti",
+              style: TextStyle(color: Colors.black),
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, 'login');
+            },
+          ),
+          const Hero(
+            tag: 2,
+            child: SizedBox(
+              height: 130,
+              child: Image(
+                image: AssetImage('assets/unina_logo.png'),
+              ),
             ),
           ),
-          Container(
-            height: 100,
-            child: Image(
-              image: AssetImage('assets/unina_logo.png'),
-            ),
-          ),
+          const SizedBox(
+            height: 10,
+          )
         ],
       ),
-    ));
+    );
   }
 }
