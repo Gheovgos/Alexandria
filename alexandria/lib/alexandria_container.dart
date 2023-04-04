e63e4662f444419b0cc92f1ca831539a6487708b
Giuseppe <snesnopic@gmail.com>
2023-04-04 16:39:34 +0200

    Widget riutilizzabile del container col bordo a bottom-left

---
+++ alexandria/lib/alexandria_container.dart
@@ -0,0 +1,21 @@
+import 'package:alexandria/constants.dart';
+import 'package:flutter/cupertino.dart';
+
+class AlexandriaContainer extends StatelessWidget {
+  AlexandriaContainer({super.key, this.width, this.child});
+  double? width;
+  Widget? child;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      decoration: const BoxDecoration(
+        color: alexandriaGreen,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(120.0),
+        ),
+      ),
+      child: child,
+    );
+  }
+}
