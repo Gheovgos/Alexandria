6cb2cd133bfc2d9bfda448c2d3ce0337a683bd46
Giuseppe <snesnopic@gmail.com>
2023-04-24 18:21:34 +0200

    Ora aprendo la tastiera non si buggerà tutto

--- alexandria/lib/login_screen.dart
+++ alexandria/lib/login_screen.dart
@@ -16,6 +16,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
