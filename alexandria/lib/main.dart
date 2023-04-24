aebd34deef64e091c9c6ec371351a7c143301b78
Giuseppe <snesnopic@gmail.com>
2023-04-24 17:00:22 +0200

    Aggiunta route per la home screen

--- alexandria/lib/main.dart
+++ alexandria/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:alexandria/login_screen.dart';
 import 'package:alexandria/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -8,7 +9,8 @@ void main() {
     initialRoute: 'welcome',
     routes: {
       'welcome': (context) => const WelcomeScreen(),
-      'login': (context) => const LoginScreen()
+      'login': (context) => const LoginScreen(),
+      'home': (context) => const HomeScreen()
     },
   ));
 }
