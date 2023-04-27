4c5862f848363c4bfe543542c091fc8c217d6c60
Giuseppe <snesnopic@gmail.com>
2023-04-27 16:30:04 +0200

    Aggiunto Settings Screen

---
+++ alexandria/lib/settings_screen.dart
@@ -0,0 +1,202 @@
+import 'package:alexandria/alexandria_rounded_button.dart';
+import 'package:alexandria/constants.dart';
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool rememberMe = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: kAlexandriaGreen,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(child: Container()),
+            Expanded(
+              flex: 39,
+              child: Row(children: [
+                const SizedBox(
+                  //spazio verde a destra
+                  width: 40,
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(70),
+                        bottomLeft: Radius.circular(70),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 8, top: 10, bottom: 10, right: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "IMPOSTAZIONI",
+                            style: TextStyle(fontSize: 24),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Nome"),
+                              TextField(
+                                style: const TextStyle(color: Colors.black),
+                                keyboardType: TextInputType.emailAddress,
+                                textAlign: TextAlign.left,
+                                onChanged: (value) {},
+                                decoration: kInputDecoration,
+                              )
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Cognome"),
+                              TextField(
+                                style: const TextStyle(color: Colors.black),
+                                keyboardType: TextInputType.name,
+                                textAlign: TextAlign.left,
+                                onChanged: (value) {},
+                                decoration: kInputDecoration,
+                              )
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Email"),
+                              TextField(
+                                keyboardType: TextInputType.emailAddress,
+                                onChanged: (value) {},
+                                decoration: kInputDecoration,
+                              )
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Username"),
+                              TextField(
+                                style: const TextStyle(color: Colors.black),
+                                keyboardType: TextInputType.name,
+                                textAlign: TextAlign.left,
+                                onChanged: (value) {},
+                                decoration: kInputDecoration,
+                              )
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Password"),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: TextField(
+                                      obscureText: true,
+                                      style:
+                                      const TextStyle(color: Colors.black),
+                                      keyboardType: TextInputType.emailAddress,
+                                      textAlign: TextAlign.left,
+                                      onChanged: (value) {},
+                                      decoration: kInputDecoration,
+                                    ),
+                                  ),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.remove_red_eye))
+                                ],
+                              )
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Conferma password"),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: TextField(
+                                      obscureText: true,
+                                      style:
+                                      const TextStyle(color: Colors.black),
+                                      keyboardType: TextInputType.emailAddress,
+                                      textAlign: TextAlign.left,
+                                      onChanged: (value) {},
+                                      decoration: kInputDecoration,
+                                    ),
+                                  ),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.remove_red_eye))
+                                ],
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                  value: rememberMe,
+                                  onChanged: (value) {
+                                    rememberMe = value!;
+                                    setState(() {});
+                                  }),
+                              const Text("Ricordami")
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              AlexandriaRoundedButton(
+                                elevation: 1,
+                                borderColor: Colors.grey,
+                                onPressed: () {},
+                                child: const Text(
+                                  "Logout",
+                                  style:
+                                  TextStyle(color: Colors.grey, fontSize: 16),
+                                ),
+                              ),
+                              AlexandriaRoundedButton(
+                                elevation: 1,
+                                borderColor: Colors.grey,
+                                onPressed: () {},
+                                child: const Text(
+                                  "Salva",
+                                  style:
+                                  TextStyle(color: Colors.grey, fontSize: 16),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+
+              ]),
+            ),
+            Expanded(child: Container())
+          ],
+        ),
+      ),
+    );
+  }
+}
