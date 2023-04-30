0fe42f40f056ca611ff8c406da2013fea56383b1
Giuseppe <snesnopic@gmail.com>
2023-04-30 12:13:32 +0200

    Aggiunta ombreggiatura, ora supporta const

--- alexandria/lib/mini_info_box.dart
+++ alexandria/lib/mini_info_box.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
 
 class MiniInfoBox extends StatelessWidget {
-  MiniInfoBox({super.key, required this.name});
-  String name;
+  const MiniInfoBox({super.key, required this.name});
+  final String name;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 5),
-      padding: const EdgeInsetsDirectional.only(
-          start: 20, top: 5, bottom: 5, end: 10),
-      decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadiusDirectional.all(
-            Radius.circular(20),
-          )),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            name,
-            style: const TextStyle(fontSize: 15),
+      child: Material(
+        elevation: 5,
+        borderRadius: const BorderRadiusDirectional.all(Radius.circular(20)),
+        child: Container(
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadiusDirectional.all(Radius.circular(20))),
+          child: Padding(
+            padding: const EdgeInsetsDirectional.only(
+                start: 20, top: 5, bottom: 5, end: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  name,
+                  style: const TextStyle(fontSize: 15),
+                ),
+                const Icon(Icons.info_outline)
+              ],
+            ),
           ),
-          const Icon(Icons.info_outline)
-        ],
+        ),
       ),
     );
   }
