141921f0f0534b6a1010dd81cb35bc8a7d7b0aed
Giuseppe <snesnopic@gmail.com>
2023-04-30 13:25:10 +0200

    Prima bozza CreateCategoryScreen

---
+++ alexandria/lib/screens/create_category_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreateCategoryScreen extends StatefulWidget {
+  const CreateCategoryScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CreateCategoryScreen> createState() => _CreateCategoryScreenState();
+}
+
+class _CreateCategoryScreenState extends State<CreateCategoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
