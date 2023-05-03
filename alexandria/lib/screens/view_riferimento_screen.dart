db941193e3c220354163a439afaa8f22ef6e0e46
Giuseppe <snesnopic@gmail.com>
2023-05-03 15:03:14 +0200

    Bozza view riferimento

---
+++ alexandria/lib/screens/view_riferimento_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ViewRiferimentoScreen extends StatefulWidget {
+  const ViewRiferimentoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ViewRiferimentoScreen> createState() => _ViewRiferimentoScreenState();
+}
+
+class _ViewRiferimentoScreenState extends State<ViewRiferimentoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
