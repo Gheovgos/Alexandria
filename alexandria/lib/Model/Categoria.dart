1360f517a242c4b0df56a174f34878eebbfa6ced
Gheovgos <giorgio.longobardo2@gmal.com>
2023-05-15 17:30:11 +0200

    Cambiato il tipo "superCategoria" da int a Categoria? per rispecchiare il json in entrata. Inoltre, è stata aggiunta la possibilità di allocare ricorsivamente questo attributo da un Json

--- alexandria/lib/Model/Categoria.dart
+++ alexandria/lib/Model/Categoria.dart
@@ -1,22 +1,43 @@
+import 'dart:convert';
 import 'dart:core';
 
 class Categoria {
+
+  Categoria(int id_categoria, String descr_categoria, int user_id,
+      Categoria? id_sopra_categoria) {
+    this.id_categoria = id_categoria;
+    this.descr_categoria = descr_categoria;
+    this.user_id = user_id;
+    this.super_Categoria = id_sopra_categoria;
+  }
+
   late int id_categoria;
   late String descr_categoria;
   late int user_id;
-  late int? id_sopra_categoria;
+  late Categoria? super_Categoria;
 
 
-  Categoria.fromJson(Map<String, dynamic>  json)
-      : id_categoria = json['id_categoria'] as int,
-        descr_categoria = json['nome'] as String,
-        user_id = json['id_utente'] as int,
-        id_sopra_categoria = json['super_Categoria'] as int?;
+  Categoria.fromJson(Map<String, dynamic> json)
+      : id_categoria = json['id_categoria'] as int {
+    if (json['nome'] != null) {
+      descr_categoria = json['nome'] as String;
+    } else {
+      descr_categoria = json['descr_categoria'] as String;
+    }
 
-  Categoria(int id_categoria, String descr_categoria, int user_id, int id_sopra_categoria) {
-    this.id_sopra_categoria = id_sopra_categoria;
-    this.user_id = user_id;
-    this.id_sopra_categoria = id_sopra_categoria;
+    if (json['id_utente'] != null) {
+      user_id = json['id_utente'] as int;
+    } else {
+      user_id = json['user_id'] as int;
+    }
+
+    if (json['super_Categoria'] != null) {
+      Map<String, dynamic> subJson = json['super_Categoria'] as Map<String, dynamic>;
+      super_Categoria = Categoria.fromJson(subJson);
+    } else {
+      super_Categoria = null;
+    }
   }
-}
 
+
+}
