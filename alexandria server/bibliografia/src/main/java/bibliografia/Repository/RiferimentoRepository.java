38cc84dd7845dbdd5fb30d45ce81f872d6956d36
Loxcudia <claudiosimonelli991@gmail.com>
2023-05-22 12:16:23 +0200

    Correzione nomi tabelle in RiferimentoRepository

--- alexandria server/bibliografia/src/main/java/bibliografia/Repository/RiferimentoRepository.java
+++ alexandria server/bibliografia/src/main/java/bibliografia/Repository/RiferimentoRepository.java
@@ -61,19 +61,19 @@ public interface RiferimentoRepository extends JpaRepository<Riferimento, Intege
     void insertRiferimentoCitante(@Param("riferimento_citato_id_riferimento") int riferimento_citato_id_riferimento, @Param("riferimento_citante_id_riferimento") int riferimento_citante_id_riferimento);
 
     @Modifying
-    @Query(value = "UPDATE autore_riferimento SET id_utente = :id_utente, id_riferimento :id_riferimento", nativeQuery = true)
+    @Query(value = "UPDATE riferimenti_biblio_user_id SET utente_user_id = :id_utente, riferimento_id_riferimento :id_riferimento", nativeQuery = true)
     @Transactional
     void updateAutore(@Param("id_utente") Integer id_utente, @Param("id_riferimento") Integer id_riferimento);
 
 
     @Modifying
-    @Query(value = "UPDATE associativa_riferimenti_categoria SET id_riferimento = :id_riferimento, id_categoria = :id_categoria", nativeQuery = true)
+    @Query(value = "UPDATE riferimenti_biblio_categorie SET riferimento_id_riferimento = :id_riferimento, categoria_id_categoria = :id_categoria", nativeQuery = true)
     @Transactional
     void updateCategoria(@Param("id_riferimento") Integer id_riferimento, @Param("id_categoria") Integer id_categoria);
 
 
     @Modifying
-    @Query(value = "UPDATE associazione_riferimenti SET id_riferimento = :id_riferimento, id_riferimento_associato = :id_riferimento_associato", nativeQuery = true)
+    @Query(value = "UPDATE riferimenti_biblio_riferimento_citante SET riferimento_citante_id_riferimento = :id_riferimento, riferimento_citato_id_riferimento = :id_riferimento_associato", nativeQuery = true)
     @Transactional
     void updateCitazione(@Param("id_riferimento") Integer id_riferimento, @Param("id_riferimento_associato") Integer id_riferimento_associato);
 }
