382bfd7d2dd1887323351fd99cf4ed851d35119e
Giuseppe <snesnopic@gmail.com>
2023-05-29 19:11:41 +0200

    Semplifico il codice di Giorgio

--- alexandria/lib/Model/Categoria.dart
+++ alexandria/lib/Model/Categoria.dart
@@ -1,26 +1,20 @@
-import 'dart:convert';
 import 'dart:core';
 
 class Categoria {
-
-  Categoria(int id_categoria, String descr_categoria, int user_id,
-      int? id_sopra_categoria) {
-    this.id_categoria = id_categoria;
-    this.nome = descr_categoria;
-    this.user_id = user_id;
-    this.super_Categoria = id_sopra_categoria;
-  }
-
-  late int id_categoria;
-  late String nome;
-  late int user_id;
-  late int? super_Categoria;
-
-
+  Categoria(
+    this.id_categoria,
+    this.nome,
+    this.user_id,
+    this.super_Categoria,
+  );
   Categoria.fromJson(Map<String, dynamic> json)
       : id_categoria = json['id_categoria'] as int,
         nome = json['descr_categoria'] as String,
         user_id = json['id_utente'] as int,
         super_Categoria = json['super_Categoria'] as int?;
 
+  late int id_categoria;
+  late String nome;
+  late int user_id;
+  int? super_Categoria;
 }
--- alexandria/lib/Model/Utente.dart
+++ alexandria/lib/Model/Utente.dart
@@ -1,62 +1,59 @@
-import 'dart:convert';
 import 'dart:core';
 import 'dart:math';
 
-
 class Utente {
+  Utente(
+    this.user_ID,
+    this.username,
+    this.nome,
+    this.cognome,
+    this.email,
+    this.password,
+  ) {
+    salt = generateRandomString();
+  }
 
-    Utente(int user_id, String username, String nome,  String cognome, String? email, String password) {
-        this.user_ID = user_id;
-        this.username = username;
-        this.nome = nome;
-        this.cognome = cognome;
-        this.email = email;
-        salt = generateRandomString();
-        this.password = password;
-    }
+  Utente.fromJson(Map<String, dynamic> json)
+      : user_ID = json['user_ID'] as int,
+        nome = json['nome'] as String,
+        cognome = json['cognome'] as String,
+        username = json['username'] as String,
+        password = json['password_hashed'] as String,
+        salt = json['salt'] as String,
+        email = json['email'] as String?;
 
-    Utente.fromJson(Map<String, dynamic>  json)
-        : user_ID = json['user_ID'] as int,
-            nome = json['nome'] as String,
-            cognome = json['cognome'] as String,
-            username = json['username'] as String,
-            password = json['password_hashed'] as String,
-            salt = json['salt'] as String,
-            email = json['email'] as String?;
+  late int user_ID;
+  late String username;
+  late String nome;
+  late String cognome;
+  String? email;
+  late String password;
+  late String salt;
 
+  Map<String, dynamic> toJson() {
+    return {
+      'user_ID': user_ID,
+      'nome': nome,
+      'cognome': cognome,
+      'email': email,
+      'username': username,
+      'password_hashed': password,
+      'salt': salt,
+    };
+  }
 
-    late int user_ID;
-    late String username;
-    late String nome;
-    late String cognome;
-    late String? email;
-    late String password;
-    late String salt;
+  String calculateHash(String unhashedPassword, String salt) {
+    int hash = unhashedPassword.hashCode ^ salt.hashCode;
+    String password_hashed = hash.toString();
 
+    return password_hashed;
+  }
 
-    Map<String, dynamic> toJson() {
-        return {
-        'user_ID': user_ID,
-        'nome': nome,
-        'cognome': cognome,
-        'email': email,
-        'username': username,
-        'password_hashed': password,
-        'salt': salt,
-        };
-    }
-
-    String calculateHash(String unhashedPassword, String salt) {
-        int hash = unhashedPassword.hashCode ^ salt.hashCode;
-        String password_hashed = hash.toString();
-
-        return password_hashed;
-    }
-
-    String generateRandomString() {
-        var len = Random();
-        var r = Random();
-        return String.fromCharCodes(List.generate(len.nextInt(20) + 5, (index) => r.nextInt(33) + 89));
-    }
-
+  String generateRandomString() {
+    final len = Random();
+    final r = Random();
+    return String.fromCharCodes(
+      List.generate(len.nextInt(20) + 5, (index) => r.nextInt(33) + 89),
+    );
+  }
 }
