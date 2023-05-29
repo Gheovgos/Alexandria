a12799d05804ac8558e253a9a7b3ded36f6b1612
Giuseppe <snesnopic@gmail.com>
2023-05-29 17:41:09 +0200

    Aggiunto costruttore "zero" perché mi serve

--- alexandria/lib/Model/Riferimento.dart
+++ alexandria/lib/Model/Riferimento.dart
@@ -2,38 +2,6 @@ import 'dart:core';
 import 'tipo_enum.dart';
 
 class Riferimento {
-  late int id_riferimento;
-  late String titolo_riferimento;
-  late DateTime data_riferimento;
-  late tipo_enum? tipo;
-  late String? URL;
-  late int? DOI;
-  late bool on_line;
-  late String? descr_riferimento;
-  late String? editore;
-  late String? isbn;
-  late String? isnn;
-  late String? luogo;
-  late int? pag_inizio;
-  late int? pag_fine;
-  late int? edizione;
-
-  Riferimento.fromJson(Map<String, dynamic>  json)
-      : id_riferimento = json['id_Rif'] as int,
-        titolo_riferimento = json['titolo'] as String,
-        data_riferimento = DateTime.parse(json['dataCreazione'] as String),
-        tipo = convertStringToEnum(json['tipo'] as String),
-        URL = json['url'] as String?,
-        DOI = json['doi'] as int?,
-        on_line = json['on_line'] as bool,
-        descr_riferimento = json['descrizione'] as String?,
-        editore = json['editore'] as String?,
-        isbn = json['isbn'] as String?,
-        isnn = json['isnn'] as String?,
-        luogo = json['luogo'] as String?,
-        pag_inizio = json['pag_inizio'] as int?,
-        pag_fine = json['pag_fine'] as int?,
-        edizione = json['edizione'] as int?;
 
   Riferimento(int id_riferimento, String titolo_riferimento, DateTime data_riferimento, tipo_enum tipo,
       String? URL, int? DOI, bool on_line, String? descr_riferimento, String? editore, String? isbn, String? isnn,
@@ -53,9 +21,46 @@ class Riferimento {
     this.pag_inizio = pag_inizio;
     this.pag_fine = pag_fine;
     this.edizione = edizione;
-
   }
 
+  Riferimento.fromJson(Map<String, dynamic>  json)
+      : id_riferimento = json['id_Rif'] as int,
+        titolo_riferimento = json['titolo'] as String,
+        data_riferimento = DateTime.parse(json['dataCreazione'] as String),
+        tipo = convertStringToEnum(json['tipo'] as String),
+        URL = json['url'] as String?,
+        DOI = json['doi'] as int?,
+        on_line = json['on_line'] as bool,
+        descr_riferimento = json['descrizione'] as String?,
+        editore = json['editore'] as String?,
+        isbn = json['isbn'] as String?,
+        isnn = json['isnn'] as String?,
+        luogo = json['luogo'] as String?,
+        pag_inizio = json['pag_inizio'] as int?,
+        pag_fine = json['pag_fine'] as int?,
+        edizione = json['edizione'] as int?;
+
+  Riferimento.empty()
+  {
+    id_riferimento = 0;
+    titolo_riferimento = '';
+    data_riferimento = DateTime.now();
+  }
+  late int id_riferimento;
+  late String titolo_riferimento;
+  late DateTime data_riferimento;
+  late tipo_enum? tipo;
+  late String? URL;
+  late int? DOI;
+  late bool on_line;
+  late String? descr_riferimento;
+  late String? editore;
+  late String? isbn;
+  late String? isnn;
+  late String? luogo;
+  late int? pag_inizio;
+  late int? pag_fine;
+  late int? edizione;
 
 
 
