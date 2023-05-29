9afce5ee7746d07289e58df071e73c3ba900d04f
Giuseppe <snesnopic@gmail.com>
2023-05-29 18:19:12 +0200

    Aggiunti altri parametri

--- alexandria/lib/components/alexandria_dialog.dart
+++ alexandria/lib/components/alexandria_dialog.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AlexandriaDialog extends StatelessWidget {
-  const AlexandriaDialog({super.key, this.content, this.actions});
+  const AlexandriaDialog({super.key, this.content, this.actions, this.title});
   final Widget? content;
   final List<Widget>? actions;
+  final Widget? title;
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      scrollable: true,
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(30)),
       ),
       actionsAlignment: MainAxisAlignment.spaceAround,
       content: content,
       actions: actions,
+      title: title,
     );
   }
 }
