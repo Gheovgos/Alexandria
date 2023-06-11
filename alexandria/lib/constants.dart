5fa5ce2e19cecd188d237a537d5532ba58709865
Giuseppe <snesnopic@gmail.com>
2023-06-11 14:01:58 +0200

    Estesa la decorazione di default per supportare lo stato disabilitato

--- alexandria/lib/constants.dart
+++ alexandria/lib/constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color kAlexandriaGreen = Color(0xFF37C534);
 const Color kInfoBoxColor = Color(0xFFF9ECEC);
+const Color kTextBoxColor = Color(0xFFE5FFE4);
 const InputDecoration kInputDecoration = InputDecoration(
   hintStyle: TextStyle(
     fontSize: 15,
@@ -10,13 +11,17 @@ const InputDecoration kInputDecoration = InputDecoration(
     ],
   ),
   filled: true,
-  fillColor: Color(0xFFE5FFE4),
+  fillColor: kTextBoxColor,
   contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
   border: OutlineInputBorder(
     borderRadius: BorderRadius.all(Radius.circular(32)),
   ),
   enabledBorder: OutlineInputBorder(
     borderSide: BorderSide(color: Colors.white, width: 0),
+    borderRadius: BorderRadius.all(Radius.circular(32)),
+  ),
+  disabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.grey, width: 0),
     borderRadius: BorderRadius.all(Radius.circular(32)),
   ),
   focusedBorder: OutlineInputBorder(
