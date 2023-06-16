4f66c15d43bf56c0fce68ac08686eef87ab44863
Giuseppe <snesnopic@gmail.com>
2023-06-16 14:20:59 +0200

    Continuati i test

--- alexandria/test/alexandria_tester.dart
+++ alexandria/test/alexandria_tester.dart
@@ -1,8 +1,5 @@
-import 'dart:ffi';
-
 import 'package:alexandria/Model/Categoria.dart';
 import 'package:alexandria/Model/Riferimento.dart';
-import 'package:alexandria/Model/Utente.dart';
 import 'package:alexandria/Model/tipo_enum.dart';
 import 'package:alexandria/globals.dart';
 import 'package:test/test.dart';
@@ -11,13 +8,11 @@ void main() {
   group('Test categorie', () {
     Categoria? prima_categoria;
     Categoria? seconda_categoria;
-    Riferimento? riferimento;
     test('Creazione categoria', () async {
-      final cat = await networkHelper.createCategoria('TestCategoria', 1, null);
-      expect('TestCategoria', cat?.nome);
-      expect(null, cat?.super_Categoria);
-      expect(1, cat?.user_id);
-      prima_categoria = cat;
+      prima_categoria = await networkHelper.createCategoria('TestCategoria', 1, null);
+      expect('TestCategoria', prima_categoria?.nome);
+      expect(null, prima_categoria?.super_Categoria);
+      expect(1, prima_categoria?.user_id);
     });
     test('Test sopra-categoria', () async {
       final cat = await networkHelper.createCategoria('TestCategoria2', 1, prima_categoria?.id_categoria);
@@ -30,41 +25,65 @@ void main() {
       final eliminaPrimaCategoria = await networkHelper.deleteCategoria(prima_categoria!);
       final eliminaSecondaCategoria = await networkHelper.deleteCategoria(seconda_categoria!);
       expect(eliminaPrimaCategoria, true);
-      expect(eliminaPrimaCategoria, false);
       expect(eliminaSecondaCategoria, true);
-      expect(eliminaSecondaCategoria, false);
-    });
-    test('Test aggiorna riferimento autore', () async {
-      riferimento = Riferimento(-1, "TestRiferimento", DateTime.now(), tipo_enum.Libro, null, null, false, "descr_riferimento", "editore", null, null, null, null, null, null);
-      final aggiornaRiferimentoAutore = await networkHelper.aggiornaRiferimentoAutore(riferimento!, 1, 2);
-      expect(aggiornaRiferimentoAutore, true);
-      expect(aggiornaRiferimentoAutore, false);
-
     });
   });
 
-  // group('Test riferimenti', ()
-  // {
-  //   Riferimento? primo_riferimento;
-  //   Riferimento? secondo_riferimento;
-  //   test('Creazione riferimento', () async {
-  //     final cat = await networkHelper.createRiferimento('TestCategoria', 1, null);
-  //     expect('TestCategoria', cat?.nome);
-  //     expect(null, cat?.super_Categoria);
-  //     expect(1, cat?.user_id);
-  //     prima_categoria = cat;
-  //   });
-  //   test('Test sopra-categoria', () async {
-  //     final cat = await networkHelper.createCategoria('TestCategoria2', 1, prima_categoria?.id_categoria);
-  //     final sopraCategorie = await networkHelper.getSopraCategorie(cat!.id_categoria);
-  //     seconda_categoria = cat;
-  //     expect(sopraCategorie?[0], prima_categoria?.id_categoria);
-  //   });
-  //   test('Test eliminazione categoria', () async {
-  //     final eliminaPrimaCategoria = await networkHelper.deleteCategoria(prima_categoria!);
-  //     final eliminaSecondaCategoria = await networkHelper.deleteCategoria(seconda_categoria!);
-  //     expect(eliminaPrimaCategoria, true);
-  //     expect(eliminaSecondaCategoria, true);
-  //   });
-  // });
+  group('Test riferimenti', () {
+    Riferimento? primo_riferimento;
+    test('Creazione riferimento', () async {
+      primo_riferimento = Riferimento(
+        0,
+        'TestRiferimento',
+        DateTime.now(),
+        tipo_enum.Conferenza,
+        null,
+        null,
+        false,
+        'descr_riferimento',
+        null,
+        null,
+        null,
+        'luogo',
+        null,
+        null,
+        null,
+      );
+      primo_riferimento =
+          await networkHelper.createRiferimento(primo_riferimento, (await networkHelper.getCategoriaById(1))!, 1);
+      expect('TestRiferimento', primo_riferimento?.titolo_riferimento);
+      expect(tipo_enum.Conferenza, primo_riferimento?.tipo);
+      expect('descr_riferimento', primo_riferimento?.descr_riferimento);
+      expect('luogo', primo_riferimento?.luogo);
+    });
+    test('Test eliminazione riferimento', () async {
+      var eliminaPrimoRiferimento = await networkHelper.deleteRiferimento(primo_riferimento!);
+      expect(eliminaPrimoRiferimento, true);
+      eliminaPrimoRiferimento = await networkHelper.deleteRiferimento(primo_riferimento!);
+      expect(eliminaPrimoRiferimento, false);
+    });
+    test('Test aggiorna riferimento autore', () async {
+      final riferimento = Riferimento(
+        -1,
+        'TestRiferimento',
+        DateTime.now(),
+        tipo_enum.Libro,
+        null,
+        null,
+        false,
+        'descr_riferimento',
+        'editore',
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+      );
+      var aggiornaRiferimentoAutore = await networkHelper.aggiornaRiferimentoAutore(riferimento, 1, 2);
+      expect(aggiornaRiferimentoAutore, true);
+      aggiornaRiferimentoAutore = await networkHelper.aggiornaRiferimentoAutore(riferimento, 2, -2);
+      expect(aggiornaRiferimentoAutore, false);
+    });
+  });
 }
